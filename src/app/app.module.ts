e26b860eb3253a0596b73845e147d9d3c07e892a
kraulain <kraulain@gmail.com>
2019-11-30 23:13:38 +0100

    add angular router using hash-location strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { HeadBarComponent } from './head-bar/head-bar.component';
 import { FootBarComponent } from './foot-bar/foot-bar.component';
 import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  // basic routes
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: 'index', component: IndexComponent },
+  { path: 'contact', component: ContactComponent }
+  //{ path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,9 +26,10 @@ import { ContactComponent } from './contact/contact.component';
     ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
